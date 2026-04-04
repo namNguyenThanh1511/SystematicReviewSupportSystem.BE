4791e35d52d45d4fc9614c8bae5188466d2a69c2
AmayaKuro <vhtkiet9615@gmail.com>
2026-04-04 12:26:37 +0700

    map fields for protocol display

--- SRSS.IAM/SRSS.IAM.Services/Mappers/ProtocolMappingProfile.cs
+++ SRSS.IAM/SRSS.IAM.Services/Mappers/ProtocolMappingProfile.cs
@@ -1,5 +1,10 @@
 using SRSS.IAM.Repositories.Entities;
+using SRSS.IAM.Services.DTOs.DataExtraction;
 using SRSS.IAM.Services.DTOs.Protocol;
+using SRSS.IAM.Services.DTOs.QualityAssessment;
+using SRSS.IAM.Services.DTOs.SearchStrategy;
+using SRSS.IAM.Services.DTOs.SelectionCriteria;
+using SRSS.IAM.Services.DTOs.Synthesis;
 
 namespace SRSS.IAM.Services.Mappers
 {
@@ -16,7 +21,15 @@ namespace SRSS.IAM.Services.Mappers
                 IsDeleted = entity.IsDeleted,
                 CreatedAt = entity.CreatedAt,
                 ApprovedAt = entity.ApprovedAt,
-                Versions = entity.Versions?.Select(v => v.ToDto()).ToList() ?? new List<VersionHistoryDto>()
+                Versions = entity.Versions?.Select(v => v.ToDto()).ToList() ?? new List<VersionHistoryDto>(),
+                SearchSources = entity.SearchSources?.Select(ss => ss.ToDto()).ToList() ?? new List<SearchSourceDto>(),
+                SelectionCriterias = entity.SelectionCriterias?.Select(sc => sc.ToDto()).ToList() ?? new List<StudySelectionCriteriaDto>(),
+                SelectionProcedures = entity.SelectionProcedures?.Select(sp => sp.ToDto()).ToList() ?? new List<StudySelectionProcedureDto>(),
+                QualityStrategies = entity.QualityStrategies?.Select(qs => qs.ToDto()).ToList() ?? new List<QualityAssessmentStrategyDto>(),
+                // ExtractionStrategies = entity.ExtractionStrategies?.Select(es => es.ToDto()).ToList() ?? new List<DataExtractionStrategyDto>(),
+                ExtractionTemplates = entity.ExtractionTemplates?.Select(et => et.ToDto()).ToList() ?? new List<ExtractionTemplateDto>(),
+                SynthesisStrategies = entity.SynthesisStrategies?.Select(ss => ss.ToDto()).ToList() ?? new List<DataSynthesisStrategyDto>(),
+                DisseminationStrategies = entity.DisseminationStrategies?.Select(ds => ds.ToDto()).ToList() ?? new List<DisseminationStrategyDto>()
             };
         }
 

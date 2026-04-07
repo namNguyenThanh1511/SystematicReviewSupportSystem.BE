84c4d1670bcc63c2231eeb9468b82fdc54c013a2
namNguyenThanh1511 <nam151103@gmail.com>
2026-04-07 17:22:57 +0700

    db context miss saving changes

---
+++ SRSS.IAM/SRSS.IAM.Repositories/Migrations/20260407075248_exclude-reason-for-study-resolution.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SRSS.IAM.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class excludereasonforstudyresolution : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExclusionReasonCode",
+                table: "screening_resolutions",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExclusionReasonCode",
+                table: "screening_resolutions");
+        }
+    }
+}

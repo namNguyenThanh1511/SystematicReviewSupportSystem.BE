602953bb527eaaf59421ada98e93739e379f564c
namNguyenThanh1511 <nam151103@gmail.com>
2026-04-09 09:49:41 +0700

    adding shortcut for power shell

---
+++ run.ps1
@@ -0,0 +1,18 @@
+﻿Push-Location "SRSS.IAM"
+
+Write-Host "Building solution..." -ForegroundColor Cyan
+dotnet build
+
+if ($?) {
+    Write-Host "--- Starting Web API ---" -ForegroundColor Green
+    try {
+        dotnet run --project "SRSS.IAM.API/SRSS.IAM.API.csproj"
+    }
+    finally {
+        Pop-Location
+        Write-Host "--- Return root folder ---" -ForegroundColor Gray
+    }
+} else {
+    Write-Host "--- Build failed ---" -ForegroundColor Red
+    Pop-Location
+}
